cee45553c38d64cb9df3ba97bc1e302a448755b0
lancibe <1609547089@qq.com>
2021-12-03 20:21:56 +0800

    DecoderLayer

--- 第七章Transformer/Transformer.py
+++ 第七章Transformer/Transformer.py
@@ -512,5 +512,74 @@ mask = Variable(torch.zeros(2, 4, 4))
 
 en = Encoder(layer, N)
 en_result = en(x, mask)
-print(en_result)
-print(en_result.shape)
+# print(en_result)
+# print(en_result.shape)
+
+
+# 使用DecoderLayer来实现解码器层
+class DecoderLayer(nn.Module):
+    def __init__(self, size, self_attn, src_attn, feed_forward, dropout):
+        """
+        初始化函数
+        :param size:词嵌入维度大小，解码器层尺寸
+        :param self_attn:多头自注意力对象，也就是这个注意力机制需要Q=K=V
+        :param src_attn:多头注意力对象，Q!=K=V
+        :param feed_forward:前馈全连接层对象
+        :param dropout:置零比率
+        """
+        super(DecoderLayer, self).__init__()
+
+        self.size = size
+        self.self_attn = self_attn
+        self.src_attn = src_attn
+        self.feed_forward = feed_forward
+        # 按照结构图克隆三个子层连接对象
+        self.sublayer = clones(SubLayerConnection(size, dropout), 3)
+
+    def forward(self, x, memory, source_mask, target_mask):
+        """
+        前向函数
+        :param x:上一层的输出
+        :param memory: 来自编码器层的语义存储变量
+        :param source_mask: 源数据掩码张量
+        :param target_mask: 目标数据掩码张量
+        :return: 该层输出
+        """
+        m = memory
+
+        # 将x传入第一个子层结构，因为是自注意力机制，所以Q，K，V都是x
+        # 最后一个参数是目标数据掩码张量，这时要对目标数据进行遮掩，因为此时模型可能还没有生成任何目标数据
+        # 比如在解码器准备生成第一个字符或词汇时，已经传入了第一个字符以便计算损失
+        # 但并不希望再生成第一个字符时模型利用这个信息，所以将其遮掩，同样在生成第二个字符或词汇时
+        # 模型只能使用第一个字符或词汇信息，第二个字符以及之后的信息都不允许被使用
+        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, target_mask))
+
+        # 第二个子层，Q是x，K、V是编码器输出memory
+        # 同样传入source_mask，但是进行源数据遮掩的原因并非抑制信息泄露，而是遮蔽对结果没用的字符产生的注意力
+        # 以此提升模型的训练速度和效果
+        x = self.sublayer[1](x, lambda x: self.src_attn(x, m, m, source_mask))
+
+        # 最后一个子层是前馈全连接子层，经过它的处理可以返回结果，这就是解码器层结构
+        return self.sublayer[2](x, self.feed_forward)
+
+
+# 类的实例化和解码器层相似，相比多出了src_attn，但是和self_attn是同一个类
+head = 8
+size = 512
+d_model = 512
+d_ff = 64
+dropout = 0.2
+self_attn = src_attn = MultiHeadedAttention(head, d_model, dropout)
+
+# 输入参数
+x = pe_result
+memory = en_result
+# 实际中source_mask和target_mask并不相同，这里为了方便计算令他们都为mask
+mask = Variable(torch.zeros(2, 4, 4))
+source_mask = target_mask = mask
+
+dl = DecoderLayer(size, self_attn, src_attn, ff, dropout)
+dl_result = dl(x, memory, source_mask, target_mask)
+print(dl_result)
+print(dl_result.shape)
+
