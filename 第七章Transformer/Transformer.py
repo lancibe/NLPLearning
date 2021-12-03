aaa9aae12a74d95005f3726fe22c51b0618b62ce
lancibe <1609547089@qq.com>
2021-12-03 21:56:42 +0800

    model

--- 第七章Transformer/Transformer.py
+++ 第七章Transformer/Transformer.py
@@ -746,7 +746,66 @@ source_mask = target_mask = Variable(torch.zeros(2, 4, 4))
 # 调用
 ed = EncoderDecoder(encoder, decoder, source_embed, target_embed, generator)
 ed_result = ed(source, target, source_mask, target_mask)
-print(ed_result)
-print(ed_result.shape)
+# print(ed_result)
+# print(ed_result.shape)
 
+
+def make_model(source_vocab, target_vocab, N=6,
+               d_model=512, d_ff=2048, head=8, dropout=0.1):
+    """
+    模型构建函数
+    :param source_vocab:源数据特征（词汇）总数
+    :param target_vocab:目标数据特征（词汇）总数
+    :param N: 编码器和解码器堆叠数
+    :param d_model: 词向量映射维度
+    :param d_ff: 前馈全连接网络中变换矩阵的维度
+    :param head: 多头注意力结构中的多头数，
+    :param dropout:置零比率
+    :return:模型的输出
+    """
+
+    # 首先得到一个深度拷贝命令，接下来很多结构都需要进行深度拷贝，来保证他们彼此之间相互独立，互不干扰
+    c = copy.deepcopy
+
+    # 实例化了很多多头注意力类，得到对象attn
+    attn = MultiHeadedAttention(head, d_model)
+
+    # 然后实例化前馈全连接类，得到对象ff
+    ff = PositionwiseFeedForward(d_model, d_ff, dropout)
+
+    # 实例化位置编码类，得到对象position
+    position = PositionalEncoding(d_model, dropout)
+
+    # 根据结构图，最外层是EncoderDecoder，在其中
+    # 分别有编码器层、解码器层、源数据Embedding层和位置编码组成的有序结构
+    # 目标数据Embedding层和位置编码组成的有序结构，以及类别生成器层
+    # 在编码器层中有attention子层以及前馈全连接子层
+    # 在解码器层中有两个attention子层以及前馈全连接层
+    model = EncoderDecoder(
+        Encoder(EncoderLayer(d_model, c(attn), c(ff), dropout), N),
+        Decoder(DecoderLayer(d_model, c(attn), c(attn),
+                             c(ff), dropout), N),
+        nn.Sequential(Embeddings(d_model, source_vocab), c(position)),
+        nn.Sequential(Embeddings(d_model, target_vocab), c(position)),
+        Generator(d_model, target_vocab)
+    )
+
+    # 模型结构完成后，接下来就是初始化模型参数，比如线性层中的变换矩阵
+    # 这里一旦判断参数维度大于1，则会将其初始化为一个服从均匀分布的矩阵
+    for p in model.parameters():
+        if p.dim() > 1:
+            nn.init.xavier_uniform_(p)
+    return model
+
+
+# 输入参数
+source_vocab = 11
+target_vocab = 11
+N = 6
+# 其他参数都是用默认值
+
+# 调用
+if __name__ == '__main__':
+    res = make_model(source_vocab, target_vocab, N)
+    print(res)
 
